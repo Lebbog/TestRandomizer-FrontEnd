da822f1691ac4c26b00706349afb0744f026dbb5
unknown <lebbos.geoffrey@gmail.com>
2019-11-21 22:26:20 -0800

    Added ability to add author through post

--- resources/JS/Authors.js
+++ resources/JS/Authors.js
@@ -1,25 +1,53 @@
 $(document).ready(function() {
-  //const cors = "https://cors-anywhere.herokuapp.com/";
-  const url = "http://localhost:8080/api/v1/testrandomizer/authors";
-  $.ajax({
-    url: url,
-    type: "GET",
-    success: function(result) {
-      fillTable(result);
-    },
-    error: function(error) {
-      console.log("Error ${error}");
+  getAuthors().done(fillTable);
+
+  $("#addAuthor").click(function() {
+    addAuthor($("#authorName").val());
+  });
+
+  $(document).on("change", ":checkbox", function() {
+    if (this.checked) {
+      var row = $(this).closest("tr")[0];
+      console.log(row.cells[2].innerHTML);
     }
   });
 });
 
+function getAuthors() {
+  const url = "http://localhost:8080/api/v1/testrandomizer/authors";
+  return $.ajax({
+    url: url,
+    type: "GET"
+  });
+}
 function fillTable(authors) {
   const tableBody = document.getElementById("authors");
   let authorsHtml = "";
+  var counter = 1;
   for (let author of authors) {
-    authorsHtml += `<tr><td>${author.name}</td><td>${author.authorId}</td></tr>`;
+    authorsHtml += ` <tr><td><input type="checkbox" id='checkbox${counter}' name="options[]" value="1"></input></td>
+    <td>${author.name}</td><td>${author.authorId}</td></tr>`;
+    counter++;
   }
   tableBody.innerHTML = authorsHtml;
 }
-// "authorId": 3,
-// "name": "Brutus"
+function addAuthor(authorName) {
+  var author = {
+    name: authorName
+  };
+  const url = "http://localhost:8080/api/v1/testrandomizer/authors";
+  $.ajax({
+    type: "POST",
+    contentType: "application/json",
+    url: url,
+    data: JSON.stringify(author),
+    dataType: "json",
+    success: function(data) {
+      console.log("Data: " + data);
+    },
+    error: function(e) {
+      alert("Error!");
+      console.log("ERROR: ", e);
+    }
+  });
+}
