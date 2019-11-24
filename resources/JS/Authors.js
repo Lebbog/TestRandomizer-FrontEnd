b5bd3c86d6bf9d0357a745485ef9137fad996525
unknown <lebbos.geoffrey@gmail.com>
2019-11-23 23:28:39 -0800

    Styling changes, Added option to delete authors

--- resources/JS/Authors.js
+++ resources/JS/Authors.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
   getAuthors().done(fillTable);
-
+  let toDelete = null;
   $("#addAuthor").click(function() {
     addAuthor($("#authorName").val());
   });
 
-  $(document).on("change", ":checkbox", function() {
-    if (this.checked) {
-      var row = $(this).closest("tr")[0];
-      console.log(row.cells[2].innerHTML);
+  $("#deleteAuthor").click(function() {
+    if (toDelete != null) {
+      console.log("about to be in delete author");
+      deleteAuthor(toDelete);
     }
+  });
+  // $(document).on("change", ":checkbox", function() {
+  //   if (this.checked) {
+  //     var row = $(this).closest("tr")[0];
+  //     console.log(row.cells[2].innerHTML);
+  //   }
+  // });
+  $(document).on("click", ".delete", function() {
+    var row = $(this).closest("tr")[0];
+    //console.log(row.cells[2].innerHTML);
+    toDelete = row.cells[2].innerHTML;
+    deleteAuthor(toDelete);
   });
 });
 
@@ -20,13 +32,14 @@ function getAuthors() {
     type: "GET"
   });
 }
+
 function fillTable(authors) {
   const tableBody = document.getElementById("authors");
   let authorsHtml = "";
   var counter = 1;
   for (let author of authors) {
-    authorsHtml += ` <tr><td><input type="checkbox" id='checkbox${counter}' name="options[]" value="1"></input></td>
-    <td>${author.name}</td><td>${author.authorId}</td></tr>`;
+    authorsHtml += `<tr><td><input type="checkbox" id='checkbox${counter}' name="options[]" value="1"></input></td>
+    <td>${author.name}</td><td>${author.authorId}</td><td><a href="#deleteAuthorModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a></td></tr>`;
     counter++;
   }
   tableBody.innerHTML = authorsHtml;
@@ -48,6 +61,19 @@ function addAuthor(authorName) {
     error: function(e) {
       alert("Error!");
       console.log("ERROR: ", e);
+    }
+  });
+}
+function deleteAuthor(authorId) {
+  console.log("in delete author");
+  const url = "/api/v1/testrandomizer/authors/" + authorId;
+  console.log(url);
+  $.ajax({
+    url: url,
+    type: "DELETE",
+    success: function(result) {
+      console.log(result);
+      toDelete = null;
     }
   });
 }
